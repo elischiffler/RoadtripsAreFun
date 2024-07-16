598e00c5f7b8eb7e02d4dae346f57fcbb1a46c3e
aidan138 <126530615+aidan138@users.noreply.github.com>
2024-07-16 13:39:37 -0700

    Added Requirements.txt and put a return type for the request

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,9 @@
-import uvicorn
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import List, Optional
-from uuid import uuid4, UUID
-from routingpy import Valhalla
-
-
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> str:
     return "Hello world"
 
