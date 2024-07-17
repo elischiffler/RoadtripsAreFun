9b0ab441e027adc71903aef254d5c984f1e2029e
aidan138 <126530615+aidan138@users.noreply.github.com>
2024-07-16 17:22:06 -0700

    Added a GET request that successfully takes query inputs

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+import routingpy as rp
+from pprint import pprint
+
 
 app = FastAPI()
+client = rp.Valhalla()
 
 
 @app.get("/")
 async def root() -> str:
     return "Hello world"
 
+
+# Route GET request
+@app.get("/get-route")
+async def get_route(*, start_lat: float, start_lon: float, end_lat: float,  end_lon: float):
+    print(start_lat, end_lat, start_lon, end_lon)
+    coords = [[start_lat, start_lon], [end_lat, end_lon]]
+    route = client.directions(locations= coords, profile='auto', dry_run=True)
+    pprint((route.geometry, route.duration, route.distance, route.raw))
+    return {"duration":route.duration, "distance":route.distance, 'geometry': route.geometry}
