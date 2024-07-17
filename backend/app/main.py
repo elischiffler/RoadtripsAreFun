c7ee07df1d05b54a37332e700bc50c141da2507a
aidan138 <126530615+aidan138@users.noreply.github.com>
2024-07-16 23:36:01 -0700

    Decided to change API's to avoid using a docker server and routingPy, which require manually parsing through a json file.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
-import routingpy as rp
-from pprint import pprint
-
+import requests
 
 app = FastAPI()
-client = rp.Valhalla()
+
 
 
 @app.get("/")
@@ -12,11 +10,31 @@ async def root() -> str:
     return "Hello world"
 
 
-# Route GET request
 @app.get("/get-route")
-async def get_route(*, start_lat: float, start_lon: float, end_lat: float,  end_lon: float):
-    print(start_lat, end_lat, start_lon, end_lon)
-    coords = [[start_lat, start_lon], [end_lat, end_lon]]
-    route = client.directions(locations= coords, profile='auto', dry_run=True)
-    pprint((route.geometry, route.duration, route.distance, route.raw))
-    return {"duration":route.duration, "distance":route.distance, 'geometry': route.geometry}
+async def get_route(*, start_lat: float, start_lon: float, end_lat: float, end_lon: float) -> dict:
+    distance, duration = 0,0
+    # Creating the get request for matbox
+    response = requests.get('https://api.mapbox.com/directions/v5/mapbox/driving/' +
+                            str(start_lon) +
+                            '%2C' +
+                            str(start_lat) +
+                            '%3B' +
+                            str(end_lon) +
+                            '%2C' +
+                            str(end_lat) +
+                            '?alternatives=false&geometries=geojson&language=en&overview=full&steps=true&access_token=pk.eyJ1IjoiYWlkYW4xMzgiLCJhIjoiY2x5cDdzODVkMDFyYjJrcHd3Z3puY2o1eSJ9.FMN-O1YsLaTjFsL4VfnufA')
+    # Need to get the distance,duration, steps
+    steps = []
+    json_data = response.json() if response and response.status_code == 200 else None
+    if json_data and 'routes' in json_data:
+        for route in json_data['routes']:
+            distance, duration = route['distance'], route['duration']
+            for leg in route['legs']:
+                for step in leg['steps']:
+                    # Each step has a distance, duration, instruction, and location for future use
+                    temp_dis = step['distance']
+                    temp_dur = step['duration']
+                    temp_instruction = step['maneuver']['instruction']
+                    temp_location = step['maneuver']['location']
+                    steps.append({'distance': temp_dis, 'duration': temp_dur, 'instruction': temp_instruction, 'location': temp_location})
+    return{'distance': distance, 'duration': duration, 'route': steps}
--- backend/tests/main_tests.py
+++ backend/tests/main_tests.py
@@ -9,3 +9,9 @@ def test_root_get():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == "Hello world"
+
+def test_get_route():
+    response = client.get("/get-route?start_lat=33.710521&start_lon=-117.763716&end_lat=33.71847966763839&end_lon=-117.92881679273557")
+    print(response.json())
+    assert response.status_code == 200
+
