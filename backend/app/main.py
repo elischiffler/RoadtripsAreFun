86c6a5cbe3fe9d4dafbcab9d4982c37ede3ae825
aidan138 <126530615+aidan138@users.noreply.github.com>
2024-07-17 12:30:24 -0700

    Improved request clarity, basic error handling, and added test cases

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 import requests
 
-app = FastAPI()
 
+app = FastAPI()
 
 
 @app.get("/")
@@ -11,30 +11,36 @@ async def root() -> str:
 
 
 @app.get("/get-route")
-async def get_route(*, start_lat: float, start_lon: float, end_lat: float, end_lon: float) -> dict:
-    distance, duration = 0,0
+async def get_route(start_lat: float, start_lon: float, end_lat: float, end_lon: float) -> dict:
     # Creating the get request for matbox
-    response = requests.get('https://api.mapbox.com/directions/v5/mapbox/driving/' +
-                            str(start_lon) +
-                            '%2C' +
-                            str(start_lat) +
-                            '%3B' +
-                            str(end_lon) +
-                            '%2C' +
-                            str(end_lat) +
-                            '?alternatives=false&geometries=geojson&language=en&overview=full&steps=true&access_token=pk.eyJ1IjoiYWlkYW4xMzgiLCJhIjoiY2x5cDdzODVkMDFyYjJrcHd3Z3puY2o1eSJ9.FMN-O1YsLaTjFsL4VfnufA')
-    # Need to get the distance,duration, steps
-    steps = []
+    url = f'https://api.mapbox.com/directions/v5/mapbox/driving/{start_lon},{start_lat};{end_lon},{end_lat}'
+    params = {
+        'alternatives': 'false',
+        'geometries': 'geojson',
+        'language': 'en',
+        'overview': 'full',
+        'steps': 'true',
+        'access_token': 'pk.eyJ1IjoiYWlkYW4xMzgiLCJhIjoiY2x5cDdzODVkMDFyYjJrcHd3Z3puY2o1eSJ9.FMN-O1YsLaTjFsL4VfnufA'
+    }
+    response = requests.get(url, params = params)
+
+    # Need to get the distance(meters), duration(seconds), steps for the route
     json_data = response.json() if response and response.status_code == 200 else None
     if json_data and 'routes' in json_data:
+        distance, duration = 0,0
+        steps = []
         for route in json_data['routes']:
             distance, duration = route['distance'], route['duration']
             for leg in route['legs']:
                 for step in leg['steps']:
                     # Each step has a distance, duration, instruction, and location for future use
-                    temp_dis = step['distance']
-                    temp_dur = step['duration']
-                    temp_instruction = step['maneuver']['instruction']
-                    temp_location = step['maneuver']['location']
-                    steps.append({'distance': temp_dis, 'duration': temp_dur, 'instruction': temp_instruction, 'location': temp_location})
-    return{'distance': distance, 'duration': duration, 'route': steps}
+                    steps.append({'distance': step['distance'],
+                                  'duration': step['duration'],
+                                  'instruction': step['maneuver']['instruction'],
+                                  'location': step['maneuver']['location']})
+        return {'coordinates': [[start_lat, start_lon], [end_lat, end_lon]],
+                'distance': distance,
+                'duration': duration,
+                'route': steps}
+    else:
+        return {"Error": "Unable to get route"}
--- backend/tests/main_tests.py
+++ backend/tests/main_tests.py
@@ -1,5 +1,4 @@
 from fastapi.testclient import TestClient
-
 from app.main import app
 
 client = TestClient(app)
@@ -10,8 +9,15 @@ def test_root_get():
     assert response.status_code == 200
     assert response.json() == "Hello world"
 
-def test_get_route():
+# A short trip test
+def test_get_route1():
     response = client.get("/get-route?start_lat=33.710521&start_lon=-117.763716&end_lat=33.71847966763839&end_lon=-117.92881679273557")
-    print(response.json())
+    print("\n", response.json())
+    print("\nThe number of steps for the trip:", len(response.json()['route']))
     assert response.status_code == 200
 
+# A cross country trip
+def test_get_route2():
+    response = client.get("/get-route?start_lat=33.710521&start_lon=-117.763716&end_lat=40.647306&end_lon=-74.157289")
+    print("\nThe number of steps for the trip:", len(response.json()['route']))
+    assert response.status_code == 200
