8294c4b48e13884dd871f8f84c5a1a350db3f247
aidan138 <126530615+aidan138@users.noreply.github.com>
2024-07-18 13:05:51 -0700

    Added a try except block to handle potential errors when getting and parsing the API response and improved test cases assertions.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from typing import Any
 import requests
-
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Route_Step(BaseModel):
+    distance: float
+    duration: float
+    instruction: str
+    location: list[float]
+
+
+class Route(BaseModel):
+    coordinates: list[Any]
+    distance: float
+    duration: float
+    route: list[Route_Step]
 
 
 @app.get("/")
@@ -10,8 +25,8 @@ async def root() -> str:
     return "Hello world"
 
 
-@app.get("/get-route")
-async def get_route(start_lat: float, start_lon: float, end_lat: float, end_lon: float) -> dict:
+@app.get("/get-route", response_model=Route)
+async def get_route(start_lat: float, start_lon: float, end_lat: float, end_lon: float) -> Any:
     # Creating the get request for matbox
     url = f'https://api.mapbox.com/directions/v5/mapbox/driving/{start_lon},{start_lat};{end_lon},{end_lat}'
     params = {
@@ -22,25 +37,31 @@ async def get_route(start_lat: float, start_lon: float, end_lat: float, end_lon:
         'steps': 'true',
         'access_token': 'pk.eyJ1IjoiYWlkYW4xMzgiLCJhIjoiY2x5cDdzODVkMDFyYjJrcHd3Z3puY2o1eSJ9.FMN-O1YsLaTjFsL4VfnufA'
     }
-    response = requests.get(url, params = params)
 
-    # Need to get the distance(meters), duration(seconds), steps for the route
-    json_data = response.json() if response and response.status_code == 200 else None
-    if json_data and 'routes' in json_data:
-        distance, duration = 0,0
-        steps = []
-        for route in json_data['routes']:
-            distance, duration = route['distance'], route['duration']
-            for leg in route['legs']:
-                for step in leg['steps']:
-                    # Each step has a distance, duration, instruction, and location for future use
-                    steps.append({'distance': step['distance'],
-                                  'duration': step['duration'],
-                                  'instruction': step['maneuver']['instruction'],
-                                  'location': step['maneuver']['location']})
-        return {'coordinates': [[start_lat, start_lon], [end_lat, end_lon]],
-                'distance': distance,
-                'duration': duration,
-                'route': steps}
-    else:
-        return {"Error": "Unable to get route"}
+    try:
+        response = requests.get(url, params=params)
+
+        # Need to get the distance(meters), duration(seconds), steps for the route
+        json_data = response.json() if response and response.status_code == 200 else None
+        if json_data and 'routes' in json_data:
+            distance, duration = 0, 0
+            steps = []
+            for route in json_data['routes']:
+                distance, duration = route['distance'], route['duration']
+                for leg in route['legs']:
+                    for step in leg['steps']:
+                        # Each step has a distance, duration, instruction, and location for future use
+                        steps.append(Route_Step(distance=step['distance'],
+                                                duration=step['duration'],
+                                                instruction=step['maneuver']['instruction'],
+                                                location=step['maneuver']['location']))
+            return Route(coordinates=[[start_lat,start_lon], [end_lat,end_lon]],
+                         distance=distance,
+                         duration=duration,
+                         route=steps)
+        else:
+            raise (HTTPException(status_code=404, detail='No routes found'))
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail=f"Mapbox request failed: {str(e)}")
+    except (KeyError, ValueError) as e:
+        raise HTTPException(status_code=500, detail=f"Error processing Mapbox response: {str(e)}")
--- backend/tests/main_tests.py
+++ backend/tests/main_tests.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.testclient import TestClient
 from app.main import app
 
@@ -9,15 +10,41 @@ def test_root_get():
     assert response.status_code == 200
     assert response.json() == "Hello world"
 
-# A short trip test
-def test_get_route1():
-    response = client.get("/get-route?start_lat=33.710521&start_lon=-117.763716&end_lat=33.71847966763839&end_lon=-117.92881679273557")
-    print("\n", response.json())
-    print("\nThe number of steps for the trip:", len(response.json()['route']))
+def test_get_route():
+    response = client.get("/get-route", params={"start_lat": 33.710521, "start_lon": -117.763716,
+                                                "end_lat": 33.71847966763839, "end_lon": -117.92881679273557})
+
     assert response.status_code == 200
+    response_data = response.json()
+    assert "coordinates" in response_data
+    assert isinstance(response_data["coordinates"], list)
+    assert "distance" in response_data
+    assert isinstance(response_data["distance"], float)
+    assert "duration" in response_data
+    assert isinstance(response_data["duration"], float)
+    assert "route" in response_data
+    assert isinstance(response_data["route"], list)
+    assert len(response_data["route"]) > 0
+
 
 # A cross country trip
+
 def test_get_route2():
-    response = client.get("/get-route?start_lat=33.710521&start_lon=-117.763716&end_lat=40.647306&end_lon=-74.157289")
-    print("\nThe number of steps for the trip:", len(response.json()['route']))
+    response = client.get("/get-route", params={"start_lat": 33.710521, "start_lon": -117.763716,
+                                                "end_lat": 40.647306, "end_lon": -74.157289})
+
     assert response.status_code == 200
+    response_data = response.json()
+    assert "coordinates" in response_data
+    assert isinstance(response_data["coordinates"], list)
+    assert "distance" in response_data
+    assert isinstance(response_data["distance"], float)
+    assert "duration" in response_data
+    assert isinstance(response_data["duration"], float)
+    assert "route" in response_data
+    assert isinstance(response_data["route"], list)
+    assert len(response_data["route"]) > 0
+
+
+if __name__ == "__main__":
+    pytest.main()
