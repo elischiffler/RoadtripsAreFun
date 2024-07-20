19d60aa740e2d4329144a582b4e21a7001576f1d
aidan138 <126530615+aidan138@users.noreply.github.com>
2024-07-20 13:24:38 -0700

    Created Base Models for Trip Advisor API

--- backend/app/models/routing_models.py
+++ backend/app/models/routing_models.py
@@ -2,11 +2,13 @@ from typing import Any, Optional
 
 from pydantic import BaseModel
 
+
 class Route_Step(BaseModel):
     distance: float
     duration: float
     instruction: str
     location: list[float]
+
 
 class Route(BaseModel):
     coordinates: list[list[float]]
@@ -14,15 +16,20 @@ class Route(BaseModel):
     duration: float
     steps: list[Route_Step]
 
+
+# Mapbox Base Models
+
 class Mapbox_waypoint(BaseModel):
     name: str
     location: list[float]
     distance: Optional[float] = None
     metadata: Optional[dict[str, Any]] = None
 
+
 class Mapbox_geo(BaseModel):
     coordinates: list[list[float]]
     type: str
+
 
 class MapBox(BaseModel):
     class MapBox_Route(BaseModel):
@@ -47,6 +54,7 @@ class MapBox(BaseModel):
                     bearing_after: int
                     bearing_before: int
                     location: list[float]
+
                 intersections: list[Any]
                 exits: Optional[str] = None
                 destinations: Optional[str] = None
@@ -68,6 +76,7 @@ class MapBox(BaseModel):
             steps: list[Mapbox_step]
             distance: float
             summary: str
+
         weight_name: str
         weight: float
         duration: float
@@ -80,3 +89,112 @@ class MapBox(BaseModel):
     waypoints: list[Mapbox_waypoint]
     code: str
     uuid: str
+
+
+# Trip Advisor API Base Models
+
+class Trip_Advisor_Error(BaseModel):
+    message: str
+    type: str
+    code: int
+
+
+class Trip_Advisor_Address(BaseModel):
+    street1: str
+    street2: str
+    city: str
+    state: str
+    country: str
+    postal_code: str
+    address_string: str
+
+
+class Trip_Advisor_Location_Search(BaseModel):
+    class Trip_Advisor_Location(BaseModel):
+        location_id: int
+        name: str
+        distance: str
+        bearing: str
+        address_obj: Trip_Advisor_Address
+
+    data: list[Trip_Advisor_Location]
+    error: Trip_Advisor_Error
+
+class Trip_Advisor_Subcategory(BaseModel):
+    name: str
+    localized_name: str
+
+class Trip_Advisor_Information(BaseModel):
+    location_id: int
+    name: str
+    description: str
+    web_url: str
+    address_obj: Trip_Advisor_Address
+    ancestors: list[dict]
+    latitude: float
+    longitude: float
+    timezone: str
+    email: str
+    phone: str
+    website: str
+    write_review: str
+    ranking_data: dict
+    rating: float
+    rating_image_url: str
+    num_reviews: str
+    review_rating_count: dict[str, str]
+    subratings: dict[str, Any]
+    photo_count: int
+    see_all_photos: str
+    price_level: str  # A number of dollar signs 1-5 indicating price
+
+    class Trip_Advisor_Hours(BaseModel):
+        class Trip_Advisor_Period(BaseModel):
+            class Trip_Advisor_Time(BaseModel):
+                day: int
+                time: str
+
+            open: Trip_Advisor_Time
+            close: Trip_Advisor_Time
+
+        periods: list[dict]
+        weekday_text: str
+
+    hours: list[Trip_Advisor_Hours]
+    amenities: list[str]
+    features: list[str]
+    cuisine: list[Trip_Advisor_Subcategory]
+    parent_brand: str
+    brand: str
+    category: Trip_Advisor_Subcategory
+    subcategory: list[Trip_Advisor_Subcategory]
+
+    class Trip_Advisor_Group(BaseModel):
+        name: str
+        localized_name: str
+        categories: list[Trip_Advisor_Subcategory]
+    groups: list[Trip_Advisor_Group]
+    styles: list[str]
+    neighborhood_info: list[Trip_Advisor_Subcategory] # List of nearby neighborhoods
+
+    class Trip_Advisor_TripType(BaseModel):
+        name: str
+        localized_name: str
+        value: str
+    trip_types: list[Trip_Advisor_TripType]
+
+    class Trip_Advisor_Award(BaseModel):
+        award_type: str
+        year: int
+
+        class Trip_Advisor_Image:
+            tiny: str
+            small: str
+            large: str
+        images: list[Trip_Advisor_Image]
+        categories: list[str]
+        display_name: str
+    awards: list[Trip_Advisor_Award]
+    error: Trip_Advisor_Error
+
+
--- backend/tests/main_tests.py
+++ backend/tests/main_tests.py
@@ -1,6 +1,6 @@
 import pytest
 from fastapi.testclient import TestClient
-from app.routers.routing_api import app
+from app.main import app
 
 
 client = TestClient(app)
