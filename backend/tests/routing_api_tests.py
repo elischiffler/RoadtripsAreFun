02bcb20522e38d41c4223440ee8d288b8f89fbd8
elischiffler <eschiffl@calpoly.edu>
2024-07-24 18:48:18 -0500

    Added test

--- backend/tests/routing_api_tests.py
+++ backend/tests/routing_api_tests.py
@@ -38,6 +38,22 @@ def test_get_route_LA_to_NY():
     assert isinstance(response_data["steps"], list)
     assert len(response_data["steps"]) > 0
 
+def test_get_route_no_stops():
+    response = client.get('/get-route', params={"start_lat": 33.710521, "start_lon": -117.763716,
+                                                "end_lat": 40.647306, "end_lon": -74.157289,"num_stops": 0 })
+    
+    assert response.status_code == 200
+    response_data = response.json()
+    assert "coordinates" in response_data
+    assert isinstance(response_data["coordinates"], list)
+    assert "distance" in response_data
+    assert isinstance(response_data["distance"], float)
+    assert "duration" in response_data
+    assert isinstance(response_data["duration"], float)
+    assert "steps" in response_data
+    assert isinstance(response_data["steps"], list)
+    assert len(response_data["steps"]) > 0
+
 
 if __name__ == "__main__":
     pytest.main()
