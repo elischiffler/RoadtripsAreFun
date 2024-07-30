e4be5c1e8e5d04f018b9376e70e4173d7b5a2c5c
elischiffler <eschiffl@calpoly.edu>
2024-07-30 16:36:47 -0500

    Added a side navigation bar with page items

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,50 @@
-import React from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import { Link } from "react-router-dom";
 
-const HomePage = () => {
-  return <p>Home Page</p>;
-};
+export default function TemporaryDrawer() {
+  const [open, setOpen] = React.useState(false);
 
-export default HomePage;
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {[
+          "Home Page",
+          "Chat Page",
+          "Itinerary Page",
+          "Login Page",
+          "Map Page",
+          "Settings Page",
+          "Login Page",
+        ].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      <Button onClick={toggleDrawer(true)}>See Pages</Button>
+      <p>Home Page</p>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+    </div>
+  );
+}
