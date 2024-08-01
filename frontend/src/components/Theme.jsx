f168ebf16af0bc2177b1ea0bc9d5043bcde4a462
elischiffler <eschiffl@calpoly.edu>
2024-08-01 16:21:33 -0500

    Made theme a component

---
+++ frontend/src/components/Theme.jsx
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material";
+
+const customTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#4464AD",
+    },
+    secondary: {
+      main: "#6DB1BF",
+    },
+    pink: {
+      main: "#E5D0E3",
+    },
+    purple: {
+      main: "#38023B",
+    },
+    dark: {
+      main: "#071108",
+    },
+  },
+});
+
+export default customTheme;
