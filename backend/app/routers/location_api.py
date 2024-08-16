47c7c9090169eb6cd9a078bd14b3bd73476b7d0b
aidan138 <126530615+aidan138@users.noreply.github.com>
2024-08-16 13:46:45 -0700

    Added comments

--- backend/app/routers/location_api.py
+++ backend/app/routers/location_api.py
@@ -8,6 +8,19 @@ router = APIRouter()
 
 @router.post("/validate-location")
 async def validate_location(request: Request) -> str:
+    """
+    Receives a json payload from the front end and validates the location specified within the payload
+
+    Parameters:
+        - request(_fastapi.Request): JSON payload received from front end in the format of location_model
+
+    Returns:
+        - str: Validated location address
+
+    Raises:
+        - HTTPException: For errors with the received payload
+
+    """
     try:
         # Convert json payload
         json_data = await request.json()
@@ -27,8 +40,10 @@ async def validate_location(request: Request) -> str:
         if location is None:
             raise HTTPException(status_code=404, detail="Location not found")
         return location.address
+    # Catch any errors accessing data that is unavailable or wrong type
     except (ValueError, KeyError) as exception:
         raise HTTPException(status_code=400, detail=f"Invalid input format: {str(exception)}")
+    # Catch errors from validating the model
     except ValidationError as exception:
         raise HTTPException(status_code=400, detail=f"Invalid payload: {str(exception)}")
 
