3d5b1f72b2ff1e64efaf0ba07a732407a931a752
aidan138 <126530615+aidan138@users.noreply.github.com>
2024-08-16 13:39:36 -0700

    Updated test cases to use payloads

--- backend/tests/location_api_tests.py
+++ backend/tests/location_api_tests.py
@@ -4,27 +4,45 @@ from app.main import app
 
 client = TestClient(app)
 
-def test_get_location_LosAngeles_address():
-    response = client.get("/validate-location", params={"address": "Los Angeles"})
+def test_post_location_LosAngeles_address():
+    payload = {"is_coordinates": False,
+                "location": {
+                    "address": "Los Angeles",
+                }
+                }
+
+    response = client.post("/validate-location", json=payload)
 
     print(response.text)
     assert response.status_code == 200
     assert response.text == '"Los Angeles, Los Angeles County, California, United States"'
 
-def test_get_location_HollyWood_Sign_coordinates():
-    response = client.get("/validate-location", params={ "coordinates": "34.133759465, -118.319665388"})
+def test_post_location_HollyWood_Sign_coordinates():
+    payload = {"is_coordinates": True,
+              "location":{
+              "coordinates": (34.133759465, -118.319665388)
+              }}
+    response = client.post("/validate-location", json=payload)
 
     assert response.status_code == 200
     assert response.text == '"Mount Lee Drive, Los Feliz Neighborhood Council District, Los Angeles, Los Angeles County, California, 90068, United States"'
 
-def test_get_location_misspell():
-    response = client.get("/validate-location", params={"address": "Los Angls"})
+def test_post_location_misspell():
+    payload= {"is_coordinates": False,
+              "location":{
+                  "address": "Los Angls"
+              }}
+    response = client.post("/validate-location", json=payload)
 
     assert response.status_code == 404
 
-def test_get_location_specific_SLO_address():
+def test_post_location_specific_SLO_address():
     address= "692 Canyon Circle, San Luis Obispo, CA, 93405"
-    response = client.get("/validate-location", params={"address":address})
+    payload = {"is_coordinates": False,
+               "location":{
+                   "address":address
+               }}
+    response = client.post("/validate-location", json=payload)
     
     assert response.status_code == 200
     assert response.text == '"Canyon Circle, Poly Canyon Village (171), San Luis Obispo County, California, 93407, United States"'
