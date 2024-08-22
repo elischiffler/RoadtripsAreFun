55104356ea5159522b9c94ff8e3ce86f5a9cfeff
aidan138 <126530615+aidan138@users.noreply.github.com>
2024-08-21 22:13:42 -0700

    Added function to sort the itinerary into dates

--- backend/app/models/itinerary_models.py
+++ backend/app/models/itinerary_models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List, Dict, Any
 
 from pydantic import BaseModel
 from .routing_models import Route
@@ -8,3 +8,7 @@ from .routing_models import Route
 class itinerary_payload(BaseModel):
     route: Route
     start_time: Optional[datetime] = datetime.now()
+
+class itinerary_day(BaseModel):
+    date: str
+    stops: [List[Dict[str, Any]]]
--- backend/app/routers/itinerary_api.py
+++ backend/app/routers/itinerary_api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException, Request
 from app.models.itinerary_models import itinerary_payload
 from datetime import timedelta
+from typing import List, Dict, Any
 
 # Initialize FastAPI
 router = APIRouter()
@@ -13,15 +14,30 @@ async def generate_itinerary(request: Request):
         json_data = await request.json()
         data = itinerary_payload.model_validate(json_data)
         current_time = data.start_time
-        itinerary = [
-            {'date-time': {'date': current_time.strftime('%A, %B %d %Y'), 'time': current_time.strftime('%H:%M')},
+        stop_list = [
+            {'date': current_time.strftime('%A, %B %d %Y'), 'time': current_time.strftime('%H:%M'),
              'name': 'Depart from your starting location'}]
         for stop in data.route.stops:
             # Add the time of the stop to the current time
             current_time = current_time + timedelta(seconds=stop['duration'])
-            itinerary.append(
-                {'date-time': {'date': current_time.strftime('%A, %B %d %Y'), 'time': current_time.strftime('%H:%M')},
+            stop_list.append(
+                {'date': current_time.strftime('%A, %B %d %Y'), # Weekday, Month Day Year
+                 'time': current_time.strftime('%H:%M'), # Hour:Minutes
                  'name': stop['name']})
+        itinerary = await _day_itinerary(stop_list)
         return itinerary
     except Exception as error:
         raise HTTPException(status_code=500, detail=f"An error occurred: {error}")
+
+async def _day_itinerary(itinerary: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    day_itinerary = []
+    curr_day = {'date': itinerary[0]['date'], 'stops': []}
+    for stop in itinerary:
+        if stop['date'] == curr_day['date']:
+            curr_day['stops'].append({'name': stop['name'], 'time': stop['time']})
+        else:
+            day_itinerary.append(curr_day)
+            curr_day = {'date': stop['date'],
+                        'stops': [{'name': stop['name'],
+                                   'time': stop['time']}]}
+    return day_itinerary
