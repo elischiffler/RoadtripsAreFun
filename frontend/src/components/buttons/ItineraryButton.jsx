3a95c48a8622599617a7371379cc173d79db8385
Eli Schiffler <eschiffler1122@gmail.com>
2024-08-27 14:56:18 -0500

    Added comments

--- frontend/src/components/buttons/ItineraryButton.jsx
+++ frontend/src/components/buttons/ItineraryButton.jsx
@@ -3,26 +3,26 @@ import { Button, Box, Typography } from "@mui/material";
 import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
 import { Link } from "react-router-dom";
 
-
+// ItineraryButton component that conditionally renders a link to the itinerary page
 const ItineraryButton = ({ itinerary }) => {
   return (
     <Link
-      to={itinerary? "/itinerary": '#'}
+      to={itinerary ? "/itinerary" : "#"} // If 'itinerary' is provided, link to the itinerary page; otherwise, use a placeholder link
       className="link"
       onClick={(e) => {
         if (!itinerary) {
-          e.preventDefault(); // Prevent routing if the value is not set
+          e.preventDefault(); // Prevent navigation if 'itinerary' is not set
         }
       }}
     >
       <Button
         variant="contained"
         className="button"
-        disabled={!itinerary}
+        disabled={!itinerary} // Disable the button if 'itinerary' is not set
       >
         <Box className="button-content">
           <FormatListBulletedIcon className="button-icon" />{" "}
-          {/* Adjust margin as needed */}
+          {/* Icon for the button, adjust margin as needed */}
           <Typography
             variant="body1"
             className="typography"
--- frontend/src/components/buttons/MapButton.jsx
+++ frontend/src/components/buttons/MapButton.jsx
@@ -3,29 +3,25 @@ import { Button, Box, Typography } from "@mui/material";
 import MapIcon from "@mui/icons-material/Map";
 import { Link } from "react-router-dom";
 
+// MapButton component that conditionally renders a link to the map page
 const MapButton = ({ route }) => {
   return (
     <Link
-      to={route? "/map": "#"}
+      to={route ? "/map" : "#"} // Prevent navigation if 'route' is not provided
       className="link"
       onClick={(e) => {
-        if (!route) {
-          e.preventDefault(); // Prevent routing if the value is not set
-        }
+        if (!route) e.preventDefault(); // Disable link if no route
       }}
     >
       <Button
         variant="contained"
         color="green"
-        startIcon={<MapIcon className="button-icon" />}
+        startIcon={<MapIcon className="button-icon" />} // Add map icon to the button
         className="button"
-        disabled={!route}
+        disabled={!route} // Disable button if no route
       >
         <Box className="button-content">
-          <Typography
-            variant="body1"
-            className="typography"
-          >
+          <Typography variant="body1" className="typography">
             Map
           </Typography>
         </Box>
