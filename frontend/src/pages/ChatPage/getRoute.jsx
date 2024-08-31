ad74b47dc45bdafe1b733ab121712ad87a40e741
Eli Schiffler <eschiffler1122@gmail.com>
2024-08-30 19:26:49 -0500

    Split getRoute into initalRoute and finalRoute

--- frontend/src/pages/ChatPage/getRoute.jsx
+++ frontend/src/pages/ChatPage/getRoute.jsx
@@ -1,9 +1,25 @@
 import axios from 'axios';
-
-export const getRoute = async (start_lat,
+export const getInitialRoute = async(start_lat, start_lon, end_lat, end_lon)=> {
+    try{
+    const params = {
+        'start_lat': start_lat,
+        'start_lon': start_lon,
+        'end_lat': end_lat,
+        'end_lon': end_lon
+    }
+    const response = await axios.get(`${import.meta.env.VITE_BACKEND_SERVER}get-initial-route`, { params: params });
+    }
+    catch(error){
+        // Log any errors encountered during the request
+        console.error("Error creating initial route:", error);
+        return null
+    }
+}
+export const getFinalRoute = async (start_lat,
     start_lon,
     end_lat,
     end_lon,
+    budget,
     stops,
 ) => {
     try{
@@ -12,22 +28,24 @@ export const getRoute = async (start_lat,
             'start_lon': start_lon,
             'end_lat': end_lat,
             'end_lon': end_lon,
+            'budget': budget,
             'num_stops': stops,
         };
 
         // Send request for a route given the user inputs
-        const response = await axios.get(`${import.meta.env.VITE_BACKEND_SERVER}get-route`, { params: params });
+        const response = await axios.get(`${import.meta.env.VITE_BACKEND_SERVER}get-final-route`, { params: params });
 
         // Access route information returned
         const route = response.data;
+        duration = route.duration
 
         // Return route structure
         console.log("Specified route:", route);
-        return route;
+        return duration;
     }
     catch(error){
         // Log any errors encountered during the request
-        console.error("Error creating a route:", error);
+        console.error("Error creating final route:", error);
         return null
     }
 };
