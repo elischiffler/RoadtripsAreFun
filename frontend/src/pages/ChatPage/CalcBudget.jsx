3f4ebc17020592e9dee4a2b8def24b36c7c84735
Eli Schiffler <eschiffler1122@gmail.com>
2024-08-30 21:17:16 -0500

    Takes a duration and gets a count of hotels

--- frontend/src/pages/ChatPage/CalcBudget.jsx
+++ frontend/src/pages/ChatPage/CalcBudget.jsx
@@ -2,8 +2,16 @@
 
 export const calcBudget = async (duration) => {
     // Generate the initial route and grab the routes duration
-
-      console.log("duration: ", duration)
-      return 0
+    let hotelCount = 0
+      if (duration < 25200){   // Can edit this number (set to 7 hours driving per day)
+        return 0
+      } else {
+        while (duration > 25200) {  // Can edit this number (set to 7 hours driving per day)
+            hotelCount ++
+            duration -= 25200  //Subtract a day worth of driving from duration
+        }
+      }
+      console.log(hotelCount)
+      return hotelCount
     }
 
