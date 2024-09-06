9d3e4b33312a7a096fcde6ddd031459ed84ccca0
aidan138 <126530615+aidan138@users.noreply.github.com>
2024-09-05 19:00:21 -0700

    Improved car_api comments

--- backend/app/routers/car_api.py
+++ backend/app/routers/car_api.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 import requests
 from requests import Response
 from requests.exceptions import RequestException
@@ -13,7 +13,7 @@ router = APIRouter()
 @router.get('/get-car-details')
 async def get_car_details(model: str, make: str, year: int) -> Dict[str, float]:
     """
-    Retrieves car details from the fuel gov on a given car's model, make, and year.
+    Retrieves car details from FuelEconomy on a given car's model, make, and year.
     
     Args:
         model (str): The model of the car
@@ -33,7 +33,6 @@ async def get_car_details(model: str, make: str, year: int) -> Dict[str, float]:
     api_url = 'https://www.fueleconomy.gov/ws/rest/vehicle/menu/options'
 
     try:
-
         model = _get_full_model_name(model , make, year)
         params = {
             'model': model,
@@ -50,12 +49,13 @@ async def get_car_details(model: str, make: str, year: int) -> Dict[str, float]:
             # Get the first available car ID for given model
             car_id = _handle_car_identifier(response)[0]
 
-            # The URL to get MPG information
-            mpg_url = f"https://www.fueleconomy.gov/ws/rest/vehicle/{car_id}"
+            # The URL to get statistics information on the car
+            statistics_url = f"https://www.fueleconomy.gov/ws/rest/vehicle/{car_id}"
 
-            # Get the car info using the id
-            car_info_response = requests.get(mpg_url)
+            # Get the car statistics xml response
+            car_info_response = requests.get(statistics_url)
             if car_info_response.status_code == requests.codes.ok:
+                # Get the combination miles per gallon from the response
                 mpg = _handle_car_info(car_info_response)
                 if isinstance(mpg, str):
                     return {'combination_mpg': float(mpg)}
@@ -64,25 +64,39 @@ async def get_car_details(model: str, make: str, year: int) -> Dict[str, float]:
 
     except RequestException as exception:
         raise HTTPException(status_code=500, detail=f"Car data request failed: {str(exception)}")
-    except ValidationError as exception:
-        raise HTTPException(status_code=502, detail=f'Improper car data response: {str(exception)}')
 
 
-def _handle_car_identifier(response: Response):
-    """Get a certain car identifier (makes or database id) from a xml response"""
+def _handle_car_identifier(response: Response) -> List[str]:
+    """
+    Get a certain car identifier (vehicle makes or database id's) from a xml response
+
+    Args:
+        - response (Response): Response from Fueleconomy API
+
+    Returns:
+        - List[str]: List of car identifiers
+    """
     print("finding id...")
     # Convert the response content to an element tree for parsing
     root = ET.fromstring(response.content)
     # Get ids list from the response
     details = [child.text for child in root.findall(".//menuItem/value")]
-    print(details)
+    # Check if details were found
     if len(details) == 0:
         raise HTTPException(status_code=404, detail="No cars found")
     return details
 
 
-def _handle_car_info(response: Response):
-    """Get the miles per gallon from information from an xml response"""
+def _handle_car_info(response: Response) -> str:
+    """
+    Get the miles per gallon from information from an xml response
+
+    Args:
+        - response (Response): Response from FuelEconomy API
+
+    Returns:
+        - The miles per gallon from information from a xml response
+    """
     root = ET.fromstring(response.content)
     # Get the vehicle type
     fuel_type = root.find('atvType').text
@@ -90,20 +104,32 @@ def _handle_car_info(response: Response):
         mpg = root.find('comb08').text  # Get the combination mpg
         return mpg
     else:
+        # Raise exception if they searched for an electric car
         raise HTTPException(status_code=500, detail="We currently do not support electric vehicles")
 
 def _get_full_model_name(model: str, make: str, year: int) -> str:
-    """Get the full model name in the database from the given model name"""
+    """
+    Get the full model name in the database from the given model name
+
+    Args:
+        model (str): The model of the car
+        make (str): The manufacturer of the car
+        year (int): The manufacturing year of the car
+
+    Returns:
+        str: The full model name in the database
+
+    """
     models_url = f"https://www.fueleconomy.gov/ws/rest/vehicle/menu/model"
     params = {
         'make': make,
         'year': year,
     }
 
-    response = requests.get(models_url, params=params)
-    if response.status_code == requests.codes.ok:
-        models = _handle_car_identifier(response)
-        for full_model in models:
-            # Check if the the provided model name is in the full_model provided
-            if model.lower() in full_model.lower():
+    response = requests.get(models_url, params=params) # Get the response from the API
+    if response.status_code == requests.codes.ok: # Ensure an OK response
+        models = _handle_car_identifier(response) # Get the list of models from xml response
+        for full_model in models: # Loop through all model names
+            # Check if the provided model name is in the full_model
+            if model.lower() in full_model.lower(): # Ensure they have the same capitalization
                 return full_model
