83c2fc8aa02cb9b55ee2e427aa467d72f10a4d79
aidan138 <126530615+aidan138@users.noreply.github.com>
2024-09-12 22:10:57 -0700

    Created Schema for chats

--- backend/app/schemas/chat_schemas.py
+++ backend/app/schemas/chat_schemas.py
@@ -1,4 +1,40 @@
 from pydantic import BaseModel
+from typing import List, Any, Optional
+from app.models.routing_models.routing_models import MapBox, Route
+
+class ChatDataSchema(BaseModel):
+    chatId: int
+    action: str
+    locationType: str
+    startCoords: List[float]
+    startAddress: str
+    endCoords: List[float]
+    endAddress: str
+    stops: int
+    showInputBar: bool
+    showStopSlider: bool
+    showBudgetSlider: bool
+    showAddressInput: bool
+    workflowStarted: bool
+    startConfirmed: Optional[Any] = None #TODO make schema for these
+    endConfirmed: Optional[Any] = None
+    initial: Optional[MapBox.MapBox_Route] = None
+    route: Optional[Route] = None
+    itinerary: Optional[Any] = None
+    loading: bool
+    hotelBudget: Optional[int] = None
+    carDetails: List[str]
+    budget: int
 
 
-# class ChatDataSchema(BaseModel):
+class ChatLogsSchema(BaseModel):
+    class ChatLogs(BaseModel):
+        class ChatMessage(BaseModel):
+            text: str
+            sender: str
+            buttons: Optional[List[Any]]
+        id: int
+        title: str
+        messages: List[ChatMessage]
+    data: List[ChatLogs]
+
